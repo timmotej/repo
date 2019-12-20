4e1196653b22fc3cb076fa6c0371439f08c636f5
Tomas Stribula <tomas.stribula@dinersclub.at>
2019-12-20 08:56:50 +0100

    INF-0 added possibility to start more containers for image

--- Docker.py
+++ Docker.py
@@ -12,36 +12,82 @@ class Docker:
         - options: e.g. { "container1":f" -i -e MYSQL_DATABASE=somedb --net={network} -p 9000:3306", "container2": ...}
         - commands: e.g. { "container1":f"ls -las /home" }
         - dockerfile_suffix: added string to Dockerfile from which to start, e.g. if you want to start from {name}-debug.Dockerfile then set to "-debug"
+        - no_containers: e.g. { "container1":3, "container2":5 ...}
     """
-    def __init__(self, names, network=None, options=None, commands=None, dockerfile_suffix=None):
+
+    def __init__(
+        self,
+        names,
+        network=None,
+        options=None,
+        commands=None,
+        dockerfile_suffix=None,
+        no_containers=None,
+    ):
         self.names = names
         self.options = (
-            {name:"" for name in self.names} if (options is None or not isinstance(options, dict)) else options
+            {name: "" for name in self.names}
+            if (options is None or not isinstance(options, dict))
+            else options
         )
         self.network = network
         self.commands = (
-            {name:"" for name in self.names} if (commands is None or not isinstance(options, dict)) else 
-                {name:commands[name] if name in list(commands.keys()) else "" for name in self.names} 
+            {name: "" for name in self.names}
+            if (commands is None or not isinstance(commands, dict))
+            else {
+                name: commands[name] if name in list(commands.keys()) else ""
+                for name in self.names
+            }
+        )
+        self.no_containers = (
+            {name: 0 for name in self.names}
+            if (no_containers is None or not isinstance(no_containers, dict))
+            else {
+                name: no_containers[name] if name in list(no_containers.keys()) else 0
+                for name in self.names
+            }
         )
         self.dockerfile_suffix = dockerfile_suffix if dockerfile_suffix else ""
 
     def debug(self):
-        [print(var) for var in [self.names, self.options, self.network, self.commands]]
+        [
+            print(var)
+            for var in [
+                self.names,
+                self.options,
+                self.network,
+                self.commands,
+                self.dockerfile_suffix,
+                self.no_containers,
+            ]
+        ]
 
     def rm_network(self):
         comms = [f"docker network rm {self.network}"]
-        return [os.system(comm) if self.network is not None else False for comm in comms]
+        return [
+            os.system(comm) if self.network is not None else False for comm in comms
+        ]
 
     def create_network(self):
         comms = [f"docker network create {self.network}"]
-        return [os.system(comm) if self.network is not None else False for comm in comms]
+        return [
+            os.system(comm) if self.network is not None else False for comm in comms
+        ]
 
     def stop_containers(self):
-        comms = [f"docker stop {name}" for name in self.names]
+        comms = [
+            f"docker stop {name}{str(no_container+1) if bool(self.no_containers[name]) else ''}"
+            for name in self.names
+            for no_container in range(self.no_containers[name])
+        ]
         return [os.system(comm) for comm in comms]
 
     def rm_containers(self):
-        comms = [f"docker rm {name}" for name in self.names]
+        comms = [
+            f"docker rm {name}{str(no_container+1) if bool(self.no_containers[name]) else ''}"
+            for name in self.names
+            for no_container in range(self.no_containers[name])
+        ]
         return [os.system(comm) for comm in comms]
 
     def rm_images(self):
@@ -51,8 +97,9 @@ class Docker:
     def build_images(self):
         # check if the dockerfile is there, otherwise it will pull from repo
         comms = [
-            f"docker build -t {name}:latest -f  {name}{self.dockerfile_suffix}.Dockerfile ." if os.path.isfile(f"{name}{self.dockerfile_suffix}.Dockerfile") 
-            else f"echo \"\'{name}{self.dockerfile_suffix}.Dockerfile\' doesn\'t exist, the \'{name}:latest\' will be downloaded from Docker repo in the docker run step"
+            f"docker build -t {name}:latest -f  {name}{self.dockerfile_suffix}.Dockerfile ."
+            if os.path.isfile(f"{name}{self.dockerfile_suffix}.Dockerfile")
+            else f"echo \"'{name}{self.dockerfile_suffix}.Dockerfile' doesn't exist, the '{name}:latest' will be downloaded from Docker repo in the docker run step"
             for name in self.names
         ]
         return [os.system(comm) for comm in comms]
@@ -63,8 +110,10 @@ class Docker:
 
     def run_containers(self):
         comms = [
-            f"docker run -d --name {name} {self.options[name]}  {name} {self.commands[name]}"
+            # if there are more containers needed to deploy, there will be run with numbers name{1,...,n}
+            f"docker run -d --name {name}{str(no_container+1) if bool(self.no_containers[name]) else ''} {self.options[name]}  {name} {self.commands[name]}"
             for name in self.names
+            for no_container in range(self.no_containers[name])
         ]
         [print(comm) for comm in comms]
         return [os.system(comm) for comm in comms]
@@ -76,15 +125,13 @@ class Docker:
         ]
         [print(comm) for comm in comms]
         return [os.system(comm) for comm in comms]
-        
+
     def run_command(self, dict_comm):
         comms = [
-            f"docker exec -it {name} sh -c \" {dict_comm[name]} \""
-            for name in self.names
+            f'docker exec -it {name} sh -c " {dict_comm[name]} "' for name in self.names
         ]
         [print(comm) for comm in comms]
         return [os.system(comm) for comm in comms]
-        
 
     def restart_containers(self):
         self.stop_containers()
--- test_start_project.py
+++ test_start_project.py
@@ -2,7 +2,7 @@
 
 from Docker import Docker, os
 
-net=input("Give here network name, e.g. empty -> produces 'empty_net':\n")
+net = input("Give here network name, e.g. empty -> produces 'empty_net':\n")
 network = f"{net}_net"
 
 dict_options = {
@@ -13,17 +13,27 @@ commands = {
     "empty": f"",
 }
 
-dockerfile="-debug"
+dockerfile = "-debug"
 
-container = Docker(list(dict_options.keys()), network, dict_options, commands, dockerfile)
+container = Docker(
+    list(dict_options.keys()), network, dict_options, commands, dockerfile
+)
 
 container.rebuild_containers()
 
 [print("docker logs " + name) for name in dict_options.keys()]
-[(print(f"\n\ndocker logs {name}\n\n"),os.system("docker logs " + name)) for name in dict_options.keys()]
+[
+    (print(f"\n\ndocker logs {name}\n\n"), os.system("docker logs " + name))
+    for name in dict_options.keys()
+]
 
 print("\n\nStatus of docker containers:\n\n")
 os.system("docker ps -a")
-[print(f"docker IP address {name}:") for name in dict_options.keys()] 
-[(print(f"\ndocker inspect {name} | grep -v '\"\"' | grep 'IPAddress\"'\n"),os.system(f"docker inspect {name} | grep -v '\"\"' | grep 'IPAddress\"'")) for name in dict_options.keys()] 
-
+[print(f"docker IP address {name}:") for name in dict_options.keys()]
+[
+    (
+        print(f"\ndocker inspect {name} | grep -v '\"\"' | grep 'IPAddress\"'\n"),
+        os.system(f"docker inspect {name} | grep -v '\"\"' | grep 'IPAddress\"'"),
+    )
+    for name in dict_options.keys()
+]
